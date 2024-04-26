7bb00f990efabdb4581b8eea8b02edc504e15155
Leo Marxs <leonardohmarques2004@gmail.com>
2024-04-26 14:52:42 -0300

    Resolução do desafio proposto pela DIO (Digital Innovation One)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("\nSmartphone Nokia:");
+
+Smartphone nokia = new Nokia(numero: "123456", Modelo: "Tijolão", IMEI: "1111111", Memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("-------------------------------------");
+
+System.Console.WriteLine("Smartphone Iphone:");
+
+Smartphone iphone = new Iphone(numero: "479722", Modelo: "15", IMEI: "2222222", Memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
